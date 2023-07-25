66df46458fd77f37669e774d32283ca3a4cbb6b5
Leonid <lkartsev698@gmail.com>
2023-07-25 22:52:15 +0300

    changed the tree, delete Dockerfile, changed the backend.tf

--- google/devops-workshop-2023/gke/backend.tf
+++ google/devops-workshop-2023/gke/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "gcs" {
-    bucket  = "bucket131"
+    bucket  = "devops-workshop-2023-terraform-state"
     prefix  = "gke"
     credentials = "./key_devops_workshop_2023.json"
   }
